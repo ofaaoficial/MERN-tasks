bb00f3d187df2e748eddc18634a40b2631d422f4
ofaaoficial <oscarfamado@gmail.com>
2019-12-01 16:26:32 -0500

    create command to execute project with nodemon and adding basic config of port, cors and json to server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(cors());
+app.use(express.json());
+
+
 
 module.exports = app;
