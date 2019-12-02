c62968e151059fc65cb4019b065ad388fb4cdc76
ofaaoficial <oscarfamado@gmail.com>
2019-12-01 19:17:41 -0500

    config react-router-dom and create routes for test and using exact in component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from "./components/Navigation";
+import TasksList from './views/task/TasksList';
+import TasksCreate from './views/task/TasksCreate';
+import TasksEdit from './views/task/TasksEdit';
 
 function App() {
   return (
-   <section>
-       <h1>Hello world!</h1>
-   </section>
+      <Router>
+          <Navigation/>
+          <Route path="/" component={TasksList} exact/>
+          <Route path="/edit/:id" component={TasksEdit}/>
+          <Route path="/create" component={TasksCreate}/>
+      </Router>
   );
 }
 
