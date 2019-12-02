f2533bc78fee400c5d405ecf09d58e2e6af1d9e4
ofaaoficial <oscarfamado@gmail.com>
2019-12-01 19:35:39 -0500

    installing bootstrap, jquery and popper.js and implementing system of router with pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from "./components/Navigation";
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min';
 import TasksList from './views/task/TasksList';
 import TasksCreate from './views/task/TasksCreate';
 import TasksEdit from './views/task/TasksEdit';
+import UserList from "./views/user/UserList";
 
 function App() {
   return (
@@ -12,6 +17,7 @@ function App() {
           <Route path="/" component={TasksList} exact/>
           <Route path="/edit/:id" component={TasksEdit}/>
           <Route path="/create" component={TasksCreate}/>
+          <Route path="/users" component={UserList}/>
       </Router>
   );
 }
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 
 export default class Navigation extends Component {
     constructor(props) {
@@ -7,7 +8,35 @@ export default class Navigation extends Component {
 
     render() {
         return (
-            <nav>Navigation</nav>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <section className="container">
+                    <Link className="navbar-brand" to="/">Navbar</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="users">Users</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Tasks
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <Link className="dropdown-item" to="/">All</Link>
+                                    <Link className="dropdown-item" to="/create">Create</Link>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+            </nav>
         )
+
     }
 }
