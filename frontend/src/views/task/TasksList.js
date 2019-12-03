86658f9a8d27ce7f6e41fb0660b417b242e1ea6b
ofaaoficial <oscarfamado@gmail.com>
2019-12-02 22:59:53 -0500

    create view for list of tasks

--- frontend/src/views/task/TasksList.js
+++ frontend/src/views/task/TasksList.js
@@ -4,14 +4,49 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.min.css';
 
 export default class TasksList extends Component {
+
     constructor(props) {
         super(props);
     }
 
+    state = {
+        tasks: []
+    };
+
+    componentDidMount() {
+        this.getTasks();
+    }
+
+    getTasks = async () => {
+        const response = await axios.get('http://localhost:4000/api/tasks');
+        this.setState({
+            tasks: response.data
+        });
+    };
+
     render() {
         return (
             <section>
-                <h1>Tasks List</h1>
+                <h1>Tasks list</h1>
+                <article className="row">
+                    {
+                        this.state.tasks.map(task =>
+                            <section className="col-md-4 p-2" key={task._id}>
+                                <article className="card">
+                                    <section className="card-header">
+                                        <h2>{task.title}</h2>
+                                    </section>
+                                    <section className="card-body">
+                                        <p>{task.content}</p>
+                                    </section>
+                                    <section className="card-footer">
+                                        <span>{task.date} - {task.author}</span>
+                                    </section>
+                                </article>
+                            </section>
+                        )
+                    }
+                </article>
             </section>
         );
     }
