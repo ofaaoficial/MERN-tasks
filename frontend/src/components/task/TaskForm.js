f52c954d6029699cba082c968cf193d104040353
ofaaoficial <oscarfamado@gmail.com>
2019-12-03 10:20:35 -0500

    implement components for him view taskcreate with taskform

--- frontend/src/components/task/TaskForm.js
+++ frontend/src/components/task/TaskForm.js
@@ -1,15 +1,119 @@
 import React, {Component} from 'react';
+import DatePicker from "react-datepicker";
+import axios from "axios";
 
 export default class TaskForm extends Component {
+
     constructor(props) {
         super(props);
     }
 
+    state = {
+        tasks: [],
+        users: [],
+
+        title: '',
+        content: '',
+        author: '',
+        date: new Date()
+    };
+
+    componentDidMount() {
+        this.getUsers();
+    }
+
+    getUsers = async () => {
+        const response = await axios.get('http://localhost:4000/api/users');
+        this.setState({
+            users: response.data.map(user => {
+                return {
+                    _id: user._id,
+                    username: user.username
+                }
+            })
+        });
+    };
+
+    onSubmit = async event => {
+        event.preventDefault();
+
+        const {title, content, author, date} = this.state;
+        const {redirect} = this.props;
+
+        const response = await axios.post('http://localhost:4000/api/tasks', {
+            title,
+            content,
+            author,
+            date
+        });
+
+        if(response.status === 201) redirect();
+        else alert(' No get data ');
+    };
+
+    handleDate = date => {
+        this.setState({date});
+    };
+
+    handleInput = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+        console.log(this.state);
+    };
+
     render() {
         return (
-            <section>
-                <h1>Task form</h1>
-            </section>
+            <form onSubmit={this.onSubmit}>
+                <section className="form-group">
+                    <label htmlFor="author">User</label>
+                    <select
+                        name="author"
+                        id="author"
+                        required
+                        className="form-control"
+                        onChange={this.handleInput}
+                    >
+                        <option value="">-- Select user --</option>
+                        {
+                            this.state.users.map(user => <option key={user._id}
+                                                                 value={user.username}>{user.username}</option>)
+                        }
+                    </select>
+                </section>
+                <section className="form-group">
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        className="form-control"
+                        onChange={this.handleInput}
+                        value={this.state.title}
+                        required
+                        placeholder="Title"/>
+                </section>
+                <section className="form-group">
+                            <textarea
+                                value={this.state.content}
+                                name="content"
+                                id="content"
+                                className="form-control"
+                                onChange={this.handleInput}
+                                placeholder="Content"
+                                cols="30" rows="10"
+                                required/>
+                </section>
+                <section className="form-group">
+                    <DatePicker
+                        className="form-control"
+                        selected={this.state.date}
+                        onChange={this.handleDate}
+                    />
+                </section>
+                <section className="form-group">
+                    <button className="btn btn-success btn-block">Register</button>
+                </section>
+            </form>
         )
     }
 }
--- frontend/src/views/task/TasksCreate.js
+++ frontend/src/views/task/TasksCreate.js
@@ -1,120 +1,20 @@
 import React, {Component} from 'react';
-import axios from "axios";
-import DatePicker from "react-datepicker";
+import TaskForm from '../../components/task/TaskForm';
 
 export default class TasksCreate extends Component {
-
-    state = {
-        tasks: [],
-        users: [],
-
-        title: undefined,
-        content: undefined,
-        author: undefined,
-        date: new Date()
-    };
-
-    componentDidMount() {
-        this.getUsers();
-    }
 
     constructor(props) {
         super(props);
     }
 
-    getUsers = async () => {
-        const response = await axios.get('http://localhost:4000/api/users');
-        this.setState({
-            users: response.data.map(user => {
-                return {
-                    _id: user._id,
-                    username: user.username
-                }
-            })
-        });
-    };
-
-    onSubmit = async event => {
-        event.preventDefault();
-
-        const {title, content, author, date} = this.state;
-
-        const response = await axios.post('http://localhost:4000/api/tasks', {
-            title,
-            content,
-            author,
-            date
-        });
-
-        if(response.status === 201)  this.props.history.push('/');
-        else alert(' No get data ');
-    };
-
-    handleDate = date => {
-        this.setState({date});
-    };
-
-    handleInput = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    };
+    redirect = () => this.props.history.push('/');
 
     render() {
         return (
             <section className="col-md-4 mx-auto">
                 <article className="card card-body">
                     <h4>Create task</h4>
-                    <form onSubmit={this.onSubmit}>
-                        <section className="form-group">
-                            <label htmlFor="author">User</label>
-                            <select
-                                name="author"
-                                id="author"
-                                required
-                                className="form-control"
-                                onChange={this.handleInput}
-                            >
-                                <option value="">-- Select user --</option>
-                                {
-                                    this.state.users.map(user => <option key={user._id}
-                                                                         value={user.username}>{user.username}</option>)
-                                }
-                            </select>
-                        </section>
-                        <section className="form-group">
-                            <input
-                                value={this.state.title}
-                                type="text"
-                                name="title"
-                                id="title"
-                                className="form-control"
-                                onChange={this.handleInput}
-                                required
-                                placeholder="Title"/>
-                        </section>
-                        <section className="form-group">
-                            <textarea
-                                value={this.state.content}
-                                name="content"
-                                id="content"
-                                className="form-control"
-                                onChange={this.handleInput}
-                                placeholder="Content"
-                                cols="30" rows="10"
-                                required/>
-                        </section>
-                        <section className="form-group">
-                            <DatePicker
-                                className="form-control"
-                                selected={this.state.date}
-                                onChange={this.handleDate}
-                            />
-                        </section>
-                        <section className="form-group">
-                            <button className="btn btn-success btn-block">Register</button>
-                        </section>
-                    </form>
+                    <TaskForm redirect={this.redirect}/>
                 </article>
             </section>
         );
